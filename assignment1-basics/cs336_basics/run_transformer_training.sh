315cbe511c698090d10e8d08d91806a61c730ca0
Kseniia Lider <not.mad.dormouse@gmail.com>
2025-10-28 20:28:21 +0000

    Change settings for training

--- assignment1-basics/cs336_basics/run_transformer_training.sh
+++ assignment1-basics/cs336_basics/run_transformer_training.sh
@@ -16,17 +16,17 @@ uv run python -m cs336_basics.train_transformer \
         --num_heads 16 \
         --batch_size 32 \
         --val_batch_size 256 \
-        --max_iters 500 \
-        --warmup_iters 20 \
-        --cosine_cycle_iters 500 \
+        --max_iters 200 \
+        --warmup_iters 10 \
+        --cosine_cycle_iters 200 \
         --learning_rate 1e-3 \
         --min_learning_rate 1e-4 \
         --beta1 0.9 \
         --beta2 0.95 \
         --grad_clip 1.0 \
         --weight_decay 0.1 \
-        --eval_and_log_interval 10 \
-        --checkpoint_interval 100 \
+        --eval_and_log_interval 5 \
+        --checkpoint_interval 50 \
         --wandb_project "cs336-assignment1" \
 
 if [ $? -eq 0 ]; then
