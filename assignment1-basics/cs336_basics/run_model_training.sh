35cf7c3054fd15474feae0eb55321f8f4790cab2
Kseniia Lider <not.mad.dormouse@gmail.com>
2025-10-31 10:40:32 +0000

    Change training settings

--- assignment1-basics/cs336_basics/run_model_training.sh
+++ assignment1-basics/cs336_basics/run_model_training.sh
@@ -6,7 +6,7 @@
 
 uv run python -m cs336_basics.train_transformer \
         --test_mode 0 \
-        --device cpu \
+        --device auto \
 
 if [ $? -eq 0 ]; then
     echo "Script executed successfully!"
